724ba08e37fbd02d93590549749714146a278095
tomoya0x03a <yamada0x03a@gmail.com>
2024-01-31 02:04:34 +0000

    リファクタリング インターフェースを実装した。

--- python/src/commands.py
+++ python/src/commands.py
@@ -1,4 +1,5 @@
 from database import DatabaseManager
+from abc import ABC, abstractmethod
 import datetime
 import sys
 import requests
@@ -6,8 +7,14 @@ import requests
 db = DatabaseManager("bookmarks.db")
 
 
-class CreateBookMarksTableCommand:
-    def execute(self):
+class Command(ABC):
+    @abstractmethod
+    def execute(self, data):
+        raise NotImplementedError("コマンドは必ずメソッドexecuteを実装してください")
+
+
+class CreateBookMarksTableCommand(Command):
+    def execute(self, data=None):
         db.create_table(
             "bookmarks",
             {
@@ -20,14 +27,14 @@ class CreateBookMarksTableCommand:
         )
 
 
-class AddBookmarkCommand:
+class AddBookmarkCommand(Command):
     def execute(self, data, timestamp=None):
         data["date_added"] = timestamp or datetime.datetime.utcnow().isoformat()
         db.add("bookmarks", data)
         return "ブックマークを追加しました。"
 
 
-class ListBookmarksCommand:
+class ListBookmarksCommand(Command):
     def __init__(self, order_by="date_added"):
         self.order_by = order_by
 
@@ -35,13 +42,13 @@ class ListBookmarksCommand:
         return db.select("bookmarks", order_by=self.order_by).fetchall()
 
 
-class DeleteBookmarkCommand:
+class DeleteBookmarkCommand(Command):
     def execute(self, data):
         db.delete("bookmarks", {"id": data})
         return "ブックマークを削除しました。"
 
 
-class EditBookmarkCommand:
+class EditBookmarkCommand(Command):
     def execute(self, data):
         db.update(
             "bookmarks",
@@ -53,12 +60,12 @@ class EditBookmarkCommand:
         return "ブックマークを編集しました。"
 
 
-class QuitCommand:
-    def execute(self):
+class QuitCommand(Command):
+    def execute(self, data=None):
         sys.exit()
 
 
-class ImportGitHubStarsCommand:
+class ImportGitHubStarsCommand(Command):
     def _extract_bookmark_info(self, repo):
         return {
             "title": repo["name"],
